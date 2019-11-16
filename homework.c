3ad25b5c47a234b70c1a6feb48266d33972b6439
sungjaemoonUCR <56247331+sungjaemoonUCR@users.noreply.github.com>
2019-11-16 02:34:54 -0600

    Update homework.c

--- homework.c
+++ homework.c
@@ -34,34 +34,42 @@ void receiveInfo(){
   hidrantes = generalInfo[2];
   fugas = generalInfo[3];
 }
-bool isCompatible(char pipe, int fila, int columna, struct Pipe *pipeType){
-  bool returnBool;
+bool isCompatible(char pipe, int fila, int columna, struct Pipe pipeType){
+  bool returnBool = true;
   // cannot go east
   if((pipe == '4' || pipe == '5'|| pipe=='6'|| pipe=='7'|| pipe=='C'|| pipe =='D'|| pipe =='E'|| pipe=='F') &&
-   (matriz[fila][columna+1] !='1'||matriz[fila][columna+1]!='3' ||  matriz[fila][columna+1]!='5'||
-   matriz[fila][columna+1]!='7' || matriz[fila][columna+1]!='9'||matriz[fila][columna+1]!='B'||matriz[fila][columna+1]!='D'||matriz[fila][columna+1]!='F')){
-    pipeType->direction = 'E';
+   (matriz[fila][columna+1] =='2'||matriz[fila][columna+1]=='4' ||  matriz[fila][columna+1]=='6'||
+   matriz[fila][columna+1]=='8'|| matriz[fila][columna+1]=='A'||matriz[fila][columna+1]=='C'||
+   matriz[fila][columna+1]=='E'||matriz[fila][columna+1]!='F'||matriz[fila][columna+1]!='0'||matriz[fila][columna+1]==-1)){
+    pipeType.direction = 'E';
+    printf("%c", pipeType.direction);
     returnBool = false;
     }
 //cannot go south
-  if((pipe =='2'||pipe=='3'||pipe == '6'||pipe=='7'||pipe=='A'||pipe=='B'||pipe == 'E'||pipe =='F')&&(matriz[fila+1][columna]!='8'
-  ||matriz[fila+1][columna]!='9'||matriz[fila+1][columna]!='A'
-  ||matriz[fila+1][columna]!='B'||matriz[fila+1][columna]!='C'||matriz[fila+1][columna]!='D'||matriz[fila+1][columna]!='E' ||matriz[fila+1][columna]!='F')){
-    pipeType->direction ='S';
+  if((pipe =='2'||pipe=='3'||pipe == '6'||pipe=='7'||pipe=='A'||pipe=='B'||pipe == 'E'||pipe =='F')&&
+  (matriz[fila+1][columna]!='8'||matriz[fila+1][columna]!='9'||matriz[fila+1][columna]!='A'
+  ||matriz[fila+1][columna]!='B'||matriz[fila+1][columna]!='C'||matriz[fila+1][columna]!='D'
+  ||matriz[fila+1][columna]!='E'||matriz[fila+1][columna]!='F'||matriz[fila+1][columna]!='0'||matriz[fila+1][columna]==-1)){
+    pipeType.direction ='S';
+    printf("%c", pipeType.direction);
     returnBool = false;
   }
   //cannot go west
-  if((pipe =='1'||pipe=='3'||pipe == '5'||pipe=='7'||pipe=='9'||pipe=='B'||pipe == 'D'||pipe == 'F')&&(matriz[fila+1][columna]!='4'
-  ||matriz[fila][columna-1]!='5'||matriz[fila+1][columna]!='6'
-  ||matriz[fila][columna-1]!='7'||matriz[fila+1][columna]!='C'||matriz[fila+1][columna]!='D'||matriz[fila+1][columna]!='E' ||matriz[fila+1][columna]!='F')){
-    pipeType->direction ='W';
+  if((pipe =='1'||pipe=='3'||pipe == '5'||pipe=='7'||pipe=='9'||pipe=='B'||pipe == 'D'||pipe == 'F')&&
+  (matriz[fila][columna-1]!='4'||matriz[fila][columna-1]!='5'||matriz[fila][columna-1]!='6'
+  ||matriz[fila][columna-1]!='7'||matriz[fila][columna-1]!='C'||matriz[fila][columna-1]!='D'
+  ||matriz[fila][columna-1]!='E' ||matriz[fila][columna-1]!='F'||matriz[fila][columna-1]!='0'||matriz[fila][columna-1]==-1)){
+    pipeType.direction ='W';
+    printf("%c", pipeType.direction);
     returnBool = false;
   }
   //cannot go north
-  if((pipe =='8'||pipe=='9'||pipe == 'A'||pipe=='B'||pipe=='C'||pipe=='D'||pipe == 'E'||pipe == 'F')&&(matriz[fila+1][columna]!='2'
-  ||matriz[fila-1][columna]!='3'||matriz[fila+1][columna]!='6'
-  ||matriz[fila-1][columna]!='7'||matriz[fila+1][columna]!='A'||matriz[fila+1][columna]!='B'||matriz[fila+1][columna]!='E' ||matriz[fila+1][columna]!='F')){
-    pipeType->direction ='N';
+  if((pipe =='8'||pipe=='9'||pipe == 'A'||pipe=='B'||pipe=='C'||pipe=='D'||pipe == 'E'||pipe == 'F')&&
+  (matriz[fila-1][columna]!='2'||matriz[fila-1][columna]!='3'||matriz[fila-1][columna]!='6'
+  ||matriz[fila-1][columna]!='7'||matriz[fila-1][columna]!='A'||matriz[fila-1][columna]!='B'
+  ||matriz[fila-1][columna]!='E' ||matriz[fila-1][columna]!='F'||matriz[fila-1][columna]!='0'||matriz[fila-1][columna]==-1)){
+    pipeType.direction ='N';
+    printf("%c", pipeType.direction);
     returnBool = false;
   }
   //cannot go north
@@ -76,33 +84,39 @@ void receiveDetails(struct Location where[]){
   }
 }
 void receiveMatrix(){
-  matriz = (char**)malloc(sizeof(char*)*filas);
-  for (int filaCounter=0; filaCounter<filas; filaCounter++){
-    matriz[filaCounter] = (char*)malloc(sizeof(char)*columnas);
+  matriz = (char**)malloc(sizeof(char*)*filas+2);
+  for (int filaCounter=0; filaCounter<filas+2; filaCounter++){
+    matriz[filaCounter] = (char*)malloc(sizeof(char)*columnas+2);
   }
 
-   for (int filaCounter= 0; filaCounter< filas; filaCounter++ ){
-    for (int columnaCounter=0; columnaCounter<columnas; columnaCounter++){
+   for (int filaCounter= 0; filaCounter< filas+2; filaCounter++ ){
+    for (int columnaCounter=0; columnaCounter<columnas+2; columnaCounter++){
+      if(filaCounter ==0 ||columnaCounter ==0 ||filaCounter ==filas+1 || columnaCounter==columnas+1){
+        matriz[filaCounter][columnaCounter] = -1;
+      }
+      else{
       scanf("%s", &matriz[filaCounter][columnaCounter]);
+      }
     }
   }
 }
-void findLeaks(struct Pipe *pipeType){
+void findLeaks(struct Pipe pipeType){
   char focus;
-  for(int filaCounter =0; filaCounter<filas; filaCounter++){
-    for(int columnaCounter=0; columnaCounter<columnas; columnaCounter++){
-      focus = matriz[filaCounter][columnaCounter];
-      if(!isCompatible(focus, filaCounter, columnaCounter, &pipeType)){
-        pipeType->hex = focus;
+  for(int filaCounter =0; filaCounter<filas+2; filaCounter++){
+    for(int columnaCounter=0; columnaCounter<columnas+2; columnaCounter++){
+      if(!isCompatible(matriz[filaCounter][columnaCounter], filaCounter, columnaCounter, pipeType)){
+        pipeType.hex = matriz[filaCounter][columnaCounter];
+        printf("%c",matriz[filaCounter][columnaCounter]);
       }
     }
   }
 }
 
+
 void printMatrix(){
   printf("\n");
-  for (int filaCounter= 0; filaCounter< filas; filaCounter++ ){
-    for (int columnaCounter=0; columnaCounter<columnas; columnaCounter++){
+  for (int filaCounter= 0; filaCounter< filas+2; filaCounter++ ){
+    for (int columnaCounter=0; columnaCounter<columnas+2; columnaCounter++){
       printf("%c", matriz[filaCounter][columnaCounter]);
     }
     printf("\n");
@@ -116,12 +130,22 @@ int main(){
   receiveInfo();
   // create struct to save variables
   struct Location where[hidrantes+fugas];
-  receiveDetails(&where);
+  receiveDetails(where);
   receiveMatrix();
-  // printMatrix();
-  findLeaks(&pipeType);
+  int dimensionCounter = 0;
+  while (dimensionCounter<hidrantes+fugas){
+    //scanf("%d %d %c",&where[dimensionCounter].row,&where[dimensionCounter].column,&where[dimensionCounter].cardinal);
+    printf("%d %d %c\n",where[dimensionCounter].row,where[dimensionCounter].column,where[dimensionCounter].cardinal);
+  dimensionCounter++;
+  }
+    printMatrix();
+  findLeaks(pipeType);
 
-for (int i =0; i<columnas; i++){
+
+
+  //findLeaks(pipeType);
+
+for (int i =0; i<filas+2; i++){
   free(matriz[i]);
 }
 free(matriz);
